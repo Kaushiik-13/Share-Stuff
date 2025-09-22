8d2e4a157b3e1d2e6db944667a5b5fa10cdb212e
Kaushiik A <akkaushiik@gmail.com>
2025-09-22 12:46:15 +0530

    rental request

--- src/rental_requests/rentalRequest.module.ts
+++ src/rental_requests/rentalRequest.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { rentalRequestService } from "./rentalRequest.service";
 import { rentalRequestController } from "./rentalRequest.controller";
 import { Item } from "src/entities/items.entity";
+import { RentalRequest } from "src/entities/rental-requests.entity";
 
 @Module({
- imports: [TypeOrmModule.forFeature([Item])],
+ imports: [TypeOrmModule.forFeature([Item,RentalRequest])],
   providers: [rentalRequestService],
   controllers: [rentalRequestController],
 })
